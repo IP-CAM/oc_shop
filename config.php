4a77a185f6e4e75c73f83576bbd5f201196181e4
Sergei <sergei.nikonov@yahoo.com>
2016-01-16 12:24:44 +0200

    open cart shop setup for all4u

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'c:/xampp/htdocs/catalog/');
+define('DIR_SYSTEM', 'c:/xampp/htdocs/system/');
+define('DIR_LANGUAGE', 'c:/xampp/htdocs/catalog/language/');
+define('DIR_TEMPLATE', 'c:/xampp/htdocs/catalog/view/theme/');
+define('DIR_CONFIG', 'c:/xampp/htdocs/system/config/');
+define('DIR_IMAGE', 'c:/xampp/htdocs/image/');
+define('DIR_CACHE', 'c:/xampp/htdocs/system/storage/cache/');
+define('DIR_DOWNLOAD', 'c:/xampp/htdocs/system/storage/download/');
+define('DIR_LOGS', 'c:/xampp/htdocs/system/storage/logs/');
+define('DIR_MODIFICATION', 'c:/xampp/htdocs/system/storage/modification/');
+define('DIR_UPLOAD', 'c:/xampp/htdocs/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
